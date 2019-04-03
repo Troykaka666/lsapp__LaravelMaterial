8fcc5da7db5de2e2e67f146c2bb514125768bf06
Troy <Troywu9527@gmail.com>
2019-04-03 17:58:22 -0400

    blade templating, compiling assests and using scss

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,12 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'LSAPP')}}</title>
-
-    </head>
-    <body>
-        <h1>Welcome to Laravel</h1>
-        <p>This is the Laravel application from the "Laravel"</p>
-    </body>
-</html>
+@section('content')
+<div class="jumbotron text-center">
+    <h1>{{ $title }}</h1>
+    <p>This is the Laravel application from the "Laravel"</p>
+    <p>
+        <a href="/login" class="btn btn-primary btn-lg" role="button">Login</a>
+        <a href="/register" class="btn btn-success btn-lg" role="button">Register</a>
+    </p>
+</div>
+@endsection
